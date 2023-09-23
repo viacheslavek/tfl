9d713cbdbc56709f6d9ce9cc10f7a38aef2e3545
Slava <Slava.lokshin@mail.ru>
2023-09-23 23:03:10 +0300

    Добавил запуск smt2 файла

--- lab1/main.go
+++ lab1/main.go
@@ -1,15 +1,21 @@
 package main
 
 import (
+	"fmt"
 	"github.com/VyacheslavIsWorkingNow/tfl/lab1/parser"
 	"os"
+	"os/exec"
 )
+
+// TODO: заменить паники на что-то более нежное
 
 func main() {
 
 	// example := "f(g(x), w(g(y), z)) -> g(f(x, y))\nh(g(x)) -> s(y)\ns(y) -> g(f(x, y))"
 
-	example := "h(x) -> s(x)"
+	// На вход подать неравенства через стрелку и знак \n
+
+	example := "h(x, y) -> s(x, y)"
 
 	report, err := parser.Parse(example)
 	if err != nil {
@@ -28,5 +34,15 @@ func main() {
 	if wErr != nil {
 		panic(wErr)
 	}
+
+	cmd := exec.Command("z3", "lab1/solver.smt2")
+
+	output, eErr := cmd.CombinedOutput()
+	if wErr != nil {
+		panic(eErr)
+	}
+
+	fmt.Println("Результат выполнения команды:")
+	fmt.Println(string(output))
 
 }
